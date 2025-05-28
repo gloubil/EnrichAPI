4760a96a87a8c4c90263413e39df4fafa19d9194
gloubil <romain.debruille40@gmail.com>
2025-05-28 15:59:48 +0200

    Remove OTX

--- sourceCode/lib/EnrichApp.py
+++ sourceCode/lib/EnrichApp.py
@@ -18,7 +18,7 @@ class EnrichApp:
         self.add(AbuseIPDB("cc83387468b1c6082cdc512fac9e3d695b7564c4545bddaf2a18a12105da400a517e2234ea98614a"))
         self.add(HybridAnalysis("blckftyz10865d30smgbd0gf38633151vpndx8cn8b5802838n7lt9ne57b0f3c1"))
         self.add(VirusTotal("1484678fe361f05164d4bbc6e1042ff201463e2977682a68a2964d3b88df661d"))
-        self.add(OTXPulse("26a50a3b86358845f731bf62a09b032eaf7a42a98da2d671522a45a160770d07"))
+        #self.add(OTXPulse("26a50a3b86358845f731bf62a09b032eaf7a42a98da2d671522a45a160770d07"))
 
     def add(self, tool):
         return self.reportGiver.register(tool)
