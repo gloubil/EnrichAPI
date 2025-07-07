67551d80fdae8fc758ab00589068253a007171bc
gloubil <romain.debruille40@gmail.com>
2025-07-07 09:18:45 +0200

    Add files via upload

--- sourceCode/lib/EnrichApp.py
+++ sourceCode/lib/EnrichApp.py
@@ -6,6 +6,8 @@ from lib.HybridAnalysis import HybridAnalysis
 from lib.VirusTotal import VirusTotal
 from lib.OTXPulse import OTXPulse
 
+from Config import Config
+
 class EnrichApp:
     """
         Classe main appelée par l'API
@@ -15,10 +17,10 @@ class EnrichApp:
         self.reportGiver = ReportGiver()
 
         # Ajoutez les EnrichTools ici avec self.add(tool)
-        self.add(AbuseIPDB("cc83387468b1c6082cdc512fac9e3d695b7564c4545bddaf2a18a12105da400a517e2234ea98614a"))
-        self.add(HybridAnalysis("blckftyz10865d30smgbd0gf38633151vpndx8cn8b5802838n7lt9ne57b0f3c1"))
-        self.add(VirusTotal("1484678fe361f05164d4bbc6e1042ff201463e2977682a68a2964d3b88df661d"))
-        self.add(OTXPulse("26a50a3b86358845f731bf62a09b032eaf7a42a98da2d671522a45a160770d07"))
+        self.add(AbuseIPDB(Config.AbuseIPDB_KEY))
+        self.add(HybridAnalysis(Config.HybridAnalisis_KEY))
+        self.add(VirusTotal(Config.VirusTotal_KEY))
+        self.add(OTXPulse(Config.OTX_KEY))
 
     def add(self, tool):
         return self.reportGiver.register(tool)
